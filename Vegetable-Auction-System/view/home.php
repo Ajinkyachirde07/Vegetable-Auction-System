5d05a946f834a0f7281f07fc2321c83fbc06b0c4
Ajinkya Chirde <ajinkyachirde07@gmail.com>
2024-07-10 11:39:12 +0530

    Add files via upload

---
+++ Vegetable-Auction-System/view/home.php
@@ -0,0 +1,229 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<?php
+		require 'view/common/head.php';
+		?>
+		
+	</head>
+	<body>
+
+		<div class="container-fluid">
+			<?php
+			require 'view/common/nav.php';
+			?>
+			<div class="carousel slide" id="carousel-578920">
+				<ol class="carousel-indicators">
+					<li data-slide-to="0" data-target="#carousel-578920" class="active"></li>
+					<li data-slide-to="1" data-target="#carousel-578920"></li>
+					<li data-slide-to="2" data-target="#carousel-578920"></li>
+					<li data-slide-to="3" data-target="#carousel-578920"></li>
+					<li data-slide-to="4" data-target="#carousel-578920"></li>
+					<li data-slide-to="5" data-target="#carousel-578920"></li>
+				</ol>
+				<div class="carousel-inner">
+					<div class="item active">
+						<img alt="Carousel Bootstrap First" src="productimages/farmar.jpg" style="width: 100%;height: 500px">
+						<div class="carousel-caption">
+							<h4>
+								
+							</h4>
+							<p>
+								
+							</p>
+						</div>
+					</div>
+					<div class="item">
+						<img alt="Carousel Bootstrap Second" src="productimages/brand1.jpg" style="width: 100%;height: 500px">
+						<div class="carousel-caption">
+							<h4>
+								
+							</h4>
+							<p>
+								
+							</p>
+						</div>
+					</div>
+					<div class="item">
+						<img alt="Carousel Bootstrap Third" src="productimages/brand3.jpg" style="width: 100%;height: 500px">
+						<div class="carousel-caption">
+							<h4>
+								
+							</h4>
+							<p>
+								
+							</p>
+						</div>
+					</div>
+					<div class="item">
+						<img alt="Carousel Bootstrap First" src="productimages/brand4.png" style="width: 100%;height: 500px">
+						<div class="carousel-caption">
+							<h4>
+								
+							</h4>
+							<p>
+								
+							</p>
+						</div>
+					</div>
+					<div class="item">
+						<img alt="Carousel Bootstrap First" src="productimages/live-024049.jpg" style="width: 100%;height: 500px">
+						<div class="carousel-caption">
+							<h4>
+								
+							</h4>
+							<p>
+								
+							</p>
+						</div>
+					</div>
+					<div class="item">
+						<img alt="Carousel Bootstrap First" src="productimages/sell.png" style="width: 100%;height: 500px">
+						<div class="carousel-caption">
+							<h4>
+								
+							</h4>
+							<p>
+								
+							</p>
+						</div>
+					</div>
+				</div>
+				<a class="left carousel-control" href="#carousel-578920" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+				<a class="right carousel-control" href="#carousel-578920" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+			</div>
+			<hr />
+			<div class="row">
+				<?php
+				require 'view/common/categories.php';
+				?>
+				<div class="col-md-10">
+					<div class="row">
+						<div class="col-md-3 col-sm-6">
+							<div class="thumbnail">
+								<img alt="Bootstrap Thumbnail First" src="productimages/tomato.jpg" style="width: 100%; height: 220px">
+								<div class="caption">
+									<h3> Tomato </h3>
+									<p>
+										
+"Discover the epitome of freshness with our plump and juicy tomatoes, handpicked at the peak of ripeness. Bursting with vibrant color and flavor, each tomato promises a tantalizing taste experience. 
+									</p>
+									<p>
+										<a class="btn btn-primary" style="width: 100%;" href="products.php?op=list&cat=1">Go!</a>
+									</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-3 col-sm-6">
+							<div class="thumbnail">
+								<img alt="Bootstrap Thumbnail First" src="productimages/onion.jpg" style="width: 100%; height: 220px">
+								<div class="caption">
+									<h3> onion  </h3>
+									<p>
+										
+"Experience culinary perfection with our premium onions, sourced from the finest fields. These crisp and flavorful bulbs add depth and aroma to any dish, whether sautéed, caramelized, or raw.
+									</p>
+									<p>
+										<a class="btn btn-primary" style="width: 100%;" href="products.php?op=list&cat=2">Go!</a>
+									</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-3 col-sm-6">
+							<div class="thumbnail">
+								<img alt="Bootstrap Thumbnail Second" src="productimages/Orange.jpg" style="width: 100%; height: 220px">
+								<div class="caption">
+									<h3> Orange </h3>
+									<p>
+									
+"Indulge in nature's sweetest treasure with our luscious oranges, bursting with vibrant citrus flavor. Each handpicked orange embodies the sun's warmth and freshness, 
+									</p>
+									<p>
+										<a class="btn btn-primary" style="width: 100%;" href="products.php?op=list&cat=3">Go!</a>
+									</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-3 col-sm-6">
+							<div class="thumbnail">
+								<img alt="Bootstrap Thumbnail Third" src="productimages/potato.jpg" style="width: 100%; height: 220px">
+								<div class="caption">
+									<h3> potato </h3>
+									<p>
+									Savor the earth's bounty with our premium potatoes, cultivated for superior taste and texture. These versatile spuds are a kitchen staple, perfect for roasting, mashing, or frying to crispy perfection. 
+									</p>
+									<p>
+										<a class="btn btn-primary" style="width: 100%;" href="products.php?op=list&cat=4">Go!</a>
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-3 col-sm-6">
+							<div class="thumbnail">
+								<img alt="Bootstrap Thumbnail First" src="productimages/banana.jpg" style="width: 100%; height: 220px">
+								<div class="caption">
+									<h3> banana </h3>
+									<p>
+									Delight in the tropical sweetness of our ripe bananas, nature's perfect snack. Each banana is meticulously selected for its creamy texture and rich flavor.
+									</p>
+									<p>
+										<a class="btn btn-primary" style="width: 100%;" href="products.php?op=list&cat=5">Go!</a>
+									</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-3 col-sm-6">
+							<div class="thumbnail">
+								<img alt="Bootstrap Thumbnail First" src="productimages/wheat.jpg" style="width: 100%; height: 220px">
+								<div class="caption">
+									<h3> wheat </h3>
+									<p>
+									Experience the wholesome goodness of our premium wheat, cultivated with care and expertise. Our grains boast exceptional quality, ideal for baking the perfect bread or crafting nutritious meals.
+									</p>
+									<p>
+										<a class="btn btn-primary" style="width: 100%;" href="products.php?op=list&cat=6">Go!</a>
+									</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-3 col-sm-6">
+							<div class="thumbnail">
+								<img alt="Bootstrap Thumbnail Second" src="productimages/kapus.jpeg" style="width: 100%; height: 220px">
+								<div class="caption">
+									<h3> kapus </h3>
+									<p>
+										Wrap yourself in luxury with our premium cotton, harvested for unparalleled softness and durability. Our carefully selected fibers offer supreme comfort and breathability
+									</p>
+									<p>
+										<a class="btn btn-primary" style="width: 100%;" href="products.php?op=list&cat=7">Go!</a>
+									</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-3 col-sm-6">
+							<div class="thumbnail">
+								<img alt="Bootstrap Thumbnail Third" src="productimages/soyabine.jpeg" style="width: 100%; height: 220px">
+								<div class="caption">
+									<h3> soyabine </h3>
+									<p>
+										
+Unlock the power of nutrition with our premium soybeans, packed with protein and essential nutrients. Grown with care and precision, our soybeans offer versatility in a variety of dishes
+									</p>
+									<p>
+										<a class="btn btn-primary" style="width: 100%;" href="products.php?op=list&cat=8">Go!</a>
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<?php
+			require 'view/common/footer.php';
+			?>
+		</div>
+
+	</body>
+</html>
